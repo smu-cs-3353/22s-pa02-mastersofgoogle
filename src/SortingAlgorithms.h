657a953ac8e7e24fb9183eabecfd0005ac1cfc68
Michael Doherty <71799695+Mikecamdo@users.noreply.github.com>
2022-03-11 12:59:46 -0600

    Fixing SortingAlgorithms.h so I can merge branches

--- src/SortingAlgorithms.h
+++ src/SortingAlgorithms.h
@@ -23,8 +23,12 @@ class SortingAlgorithms {
         //Merge function for MergeSort
         void merge(T[], int, int, int);
 
-        //Private, recursive IntroSort function
+        //Recursive IntroSort function
         void introSort(T[], T*, T*, int);
+
+        //HeapSort implementation for IntroSort
+        void heapify(T[], int, int);
+        void heapSort(T[], T*, T*);
 
         //Sorting Algorithms
         void insertionSort(T[], int, int);
@@ -194,15 +198,15 @@ void SortingAlgorithms<T>::sortDatasets(const string& filePath) {
 //Inspired by code found at: https://www.geeksforgeeks.org/insertion-sort/ and https://www.techiedelight.com/introsort-algorithm/
 template <typename T>
 void SortingAlgorithms<T>::insertionSort(T theArray[], int low, int high) {
-    for (int i = low + 1; i <= high; i++) {
+    for (int i = low + 1; i <= high; i++) { //start from second element (as first element is already sorted)
         T key = theArray[i];
         int j = i - 1;
 
-        while (j >= low && theArray[j] > key) {
+        while (j >= low && theArray[j] > key) { //find index where theArray[i] belongs
             theArray[j + 1] = theArray[j];
             j -= 1;
         }
-        theArray[j + 1] = key;
+        theArray[j + 1] = key; //insert theArray[i] where it belongs
     }
 }
 
@@ -210,26 +214,26 @@ void SortingAlgorithms<T>::insertionSort(T theArray[], int low, int high) {
 template <typename T>
 void SortingAlgorithms<T>::randomizedQuickSort(T theArray[], int low, int high) {
     if (low < high) {
-        int partitionIndex = partition(theArray, low, high);
+        int partitionIndex = partition(theArray, low, high); //finds the point to partition around
 
-        randomizedQuickSort(theArray, low, partitionIndex - 1);
-        randomizedQuickSort(theArray, partitionIndex + 1, high);
+        randomizedQuickSort(theArray, low, partitionIndex - 1); //sort elements before partition
+        randomizedQuickSort(theArray, partitionIndex + 1, high); //sort elements after partition
     }
 }
 
 template <typename T>
 int SortingAlgorithms<T>::partition(T theArray[], int low, int high) {
-    int randomNum = (rand() % (high - low)) + low;
+    int randomNum = (rand() % (high - low)) + low; //generates a random index
 
-    swap(&theArray[randomNum], &theArray[high]);
+    swap(&theArray[randomNum], &theArray[high]); //swaps the value of the last element with the value of the random index's element
 
-    T pivot = theArray[high];
+    T pivot = theArray[high]; //pivot is now the random index's element (which was moved to theArray[high])
     int i = low - 1;
 
     for (int j = low; j <= high - 1; j++) {
-        if (theArray[j] <= pivot) {
+        if (theArray[j] <= pivot) { //if current element is less than or equal to the pivot
             i++;
-            swap(&theArray[i], &theArray[j]);
+            swap(&theArray[i], &theArray[j]); //swap the elements
         }
     }
 
@@ -238,7 +242,7 @@ int SortingAlgorithms<T>::partition(T theArray[], int low, int high) {
 }
 
 template <typename T>
-void SortingAlgorithms<T>::swap(T* a, T* b) {
+void SortingAlgorithms<T>::swap(T* a, T* b) { //swaps the two values passed to it
     T temp = *a;
     *a = *b;
     *b = temp;
@@ -251,12 +255,12 @@ void SortingAlgorithms<T>::mergeSort(T theArray[], int left, int right) {
         return;
     }
 
-    int middle = left + (right - left) / 2;
+    int middle = left + (right - left) / 2; //middle of the array
 
-    mergeSort(theArray, left, middle);
-    mergeSort(theArray, middle + 1, right);
+    mergeSort(theArray, left, middle); //the left half of the array
+    mergeSort(theArray, middle + 1, right); //the right half of the array
 
-    merge(theArray, left, middle, right);
+    merge(theArray, left, middle, right); //recombines sorted halves
 
 }
 
@@ -265,23 +269,23 @@ void SortingAlgorithms<T>::merge(T theArray[], int left, int middle, int right) 
     int subArray1 = middle - left + 1;
     int subArray2 = right - middle;
 
-    T* leftArray = new T[subArray1];
+    T* leftArray = new T[subArray1]; //temporary arrays
     T* rightArray = new T[subArray2];
 
-    for (int i = 0; i < subArray1; i++) {
+    for (int i = 0; i < subArray1; i++) { //copies data to temporary array
         leftArray[i] = theArray[left + i];
     }
 
-    for (int i = 0; i < subArray2; i++) {
+    for (int i = 0; i < subArray2; i++) { //copies data to temporary array
         rightArray[i] = theArray[middle + i + 1];
     }
 
-    int subIndex1 = 0;
-    int subIndex2 = 0;
+    int subIndex1 = 0; //index of first subarray (the left subarray)
+    int subIndex2 = 0; //index of second subarray (the right subarray)
 
-    int mergedIndex = left;
+    int mergedIndex = left; //index of the merged array
 
-    while (subIndex1 < subArray1 && subIndex2 < subArray2) {
+    while (subIndex1 < subArray1 && subIndex2 < subArray2) { //adds elements to the merged array in ascending order
         if (leftArray[subIndex1] <= rightArray[subIndex2]) {
             theArray[mergedIndex] = leftArray[subIndex1];
             subIndex1++;
@@ -292,20 +296,20 @@ void SortingAlgorithms<T>::merge(T theArray[], int left, int middle, int right) 
         mergedIndex++;
     }
 
-    while (subIndex1 < subArray1) {
+    while (subIndex1 < subArray1) { //adds any leftover elements from the left array to the merged array
         theArray[mergedIndex] = leftArray[subIndex1];
         subIndex1++;
         mergedIndex++;
     }
 
-    while (subIndex2 < subArray2) {
+    while (subIndex2 < subArray2) { //adds any leftover elements from the right array to the merged array
         theArray[mergedIndex] = rightArray[subIndex2];
         subIndex2++;
         mergedIndex++;
     }
 
-    delete[] leftArray;
-    delete[] rightArray;
+    delete[] leftArray; //deletes the temporary arrays
+    delete[] rightArray; //deletes the temporary arrays
 }
 
 //Inspired by pseudo-code found at: https://www.tutorialspoint.com/data_structures_algorithms/shell_sort_algorithm.htm
@@ -313,54 +317,101 @@ template <typename T>
 void SortingAlgorithms<T>::shellSort(T theArray[], int size) {
     int interval = 1;
 
-    while (interval < size / 3) {
+    while (interval < size / 3) { //calculates the interval
         interval = interval * 3 + 1;
     }
 
     while (interval > 0) {
         for (int i = interval; i < size; i++) {
-            T value = theArray[i];
+            T value = theArray[i]; //value to be inserted
             int j = i;
 
-            while (j >= interval && theArray[j - interval] > value) {
+            while (j >= interval && theArray[j - interval] > value) { //shifts elements right
                 theArray[j] = theArray[j - interval];
                 j = j - interval;
             }
 
-            theArray[j] = value;
+            theArray[j] = value; //inserts the selected value
         }
 
-        interval = (interval - 1) / 3;
+        interval = (interval - 1) / 3; //decrements the interval
     }
 }
 
 //Inspired by code found at: https://www.techiedelight.com/introsort-algorithm/
 template <typename T>
 void SortingAlgorithms<T>::introSort(T theArray[], T* beginning, T* end) {
-    int depthLimit = 2 * log(end - beginning);
+    int depthLimit = 2 * log(end - beginning); //calculates the maximum depth
 
-    introSort(theArray, beginning, end, depthLimit);
+    introSort(theArray, beginning, end, depthLimit); //calls the recursive introSort function
 }
 
 template <typename T>
 void SortingAlgorithms<T>::introSort(T theArray[], T* beginning, T* end, int depthLimit) {
-    int size = end - beginning;
+    int size = end - beginning; //size of the subarray
 
-    if (size < 16) {
+    if (size < 16) { //if size is small enough, use insertionSort to sort the subarray
         insertionSort(theArray, beginning - theArray, end - theArray);
         return;
-    } else if (depthLimit == 0) {
-        make_heap(beginning, end + 1);
-        sort_heap(beginning, end + 1);
+    } else if (depthLimit == 0) { //if the depthLimit is reached, then sort the subarray with heapSort
+        T copy[size + 1]; //temporary copy of the part of the array that needs to be sorted
+        int j = 0;
+        for (int i = beginning - theArray; i < end - theArray + 1; i++) {
+            copy[j] = theArray[i]; //copies that part of the array that needs to be sorted
+            j++;
+        }
+
+        heapSort(copy, beginning, end + 1); //call heapSort on the part of the array that needs to be sorted
+
+        j = 0;
+        for (int i = beginning - theArray; i < end - theArray + 1; i++) {
+            theArray[i] = copy[j]; //copies the newly sorted part of the array back to the original array
+            j++;
+        }
         return;
-    } else {
+    } else { //Sort the array using quickSort
 
-        T* partitionIndex = theArray + partition(theArray, beginning - theArray, end - theArray); //introSortPartition(theArray, beginning - theArray, end - theArray);
+        T* partitionIndex = theArray + partition(theArray, beginning - theArray, end - theArray); //find the partition
 
-        introSort(theArray, beginning, partitionIndex - 1, depthLimit - 1);
-        introSort(theArray, partitionIndex + 1, end, depthLimit - 1);
+        introSort(theArray, beginning, partitionIndex - 1, depthLimit - 1); //sort elements before partition
+        introSort(theArray, partitionIndex + 1, end, depthLimit - 1); //sort elements after partition
 
         return;
+    }
+}
+
+//Inspired by code found at: https://www.geeksforgeeks.org/heap-sort/
+template<typename T>
+void SortingAlgorithms<T>::heapSort(T theArray[], T* beginning, T* end) {
+    int size = end - beginning; //size of the array/subarray
+
+    for (int i = size / 2 - 1; i >= 0; i--) { //builds the heap
+        heapify(theArray, size, i);
+    }
+
+    for (int i = size - 1; i >= 0; i--) { //extracts elements from the heap
+        swap(&theArray[0], &theArray[i]); //move root to the end
+        heapify(theArray, i, 0); //heapify the newly reduced heap
+    }
+}
+
+template<typename T>
+void SortingAlgorithms<T>::heapify(T theArray[], int size, int i) {
+    int largest = i; //largest is initially the root
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < size && theArray[left] > theArray[largest]) { //if left is larger than the root
+        largest = left;
+    }
+
+    if (right < size && theArray[right] > theArray[largest]) { //if right is larger than the left and the root
+        largest = right;
+    }
+
+    if (largest != i) { //if largest was changed to either left or right
+        swap(&theArray[i], &theArray[largest]); //swap values
+        heapify(theArray, size, largest); //heapify the subtree
     }
 }
 
@@ -368,17 +419,17 @@ void SortingAlgorithms<T>::introSort(T theArray[], T* beginning, T* end, int dep
 template <typename T>
 void SortingAlgorithms<T>::timSort(T theArray[], int size) {
     int run = 32;
-    for (int i = 0; i < size; i += run) { //FIXME can change 32 to 64 (or a larger power of 2)
-        insertionSort(theArray, i, std::min((i + run - 1), (size - 1))); //FIXME change 32 here too
+    for (int i = 0; i < size; i += run) { //sort subarrays (with each subarray having 32 elements) using insertion sort
+        insertionSort(theArray, i, std::min((i + run - 1), (size - 1)));
     }
 
-    for (int i = run; i < size; i *= 2) {
+    for (int i = run; i < size; i *= 2) { //merge the subarrays (of increasingly larger sizes)
         for (int left = 0; left < size; left += i * 2) {
-            int middle = left + i - 1;
-            int right = std::min((left + 2 * i - 1), (size - 1));
+            int middle = left + i - 1; //endpoint of left subarray
+            int right = std::min((left + 2 * i - 1), (size - 1)); //endpoint of right subarray
 
             if (middle < right) {
-                merge(theArray, left, middle, right);
+                merge(theArray, left, middle, right); //merge the subarrays
             }
         }
     }
